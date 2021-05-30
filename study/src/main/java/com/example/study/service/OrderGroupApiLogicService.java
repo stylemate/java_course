c2da1b1ffeb57bc68b6eb1d318d19dd06cd4023b
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-05-30 11:50:33 +0900

    orderGroup API finished

---
+++ study/src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -0,0 +1,106 @@
+package com.example.study.service;
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.OrderGroup;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.OrderGroupApiRequest;
+import com.example.study.model.network.response.OrderGroupApiResponse;
+import com.example.study.repository.OrderGroupRepository;
+import com.example.study.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    private OrderGroupRepository orderGroupRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    //fetch req data
+    @Override
+    public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
+
+        OrderGroupApiRequest requestBody = request.getData();
+
+        OrderGroup orderGroup = OrderGroup.builder()
+                .status(requestBody.getStatus())
+                .orderType(requestBody.getOrderType())
+                .revAddress(requestBody.getRevAddress())
+                .revName(requestBody.getRevName())
+                .paymentType(requestBody.getPaymentType())
+                .totalPrice(requestBody.getTotalPrice())
+                .totalQuantity(requestBody.getTotalQuantity())
+                .orderAt(LocalDateTime.now())
+//                .arrivalDate(requestBody.getArrivalDate())
+                .user(userRepository.getOne(requestBody.getUserId()))
+                .build();
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+
+        return response(newOrderGroup);
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> read(Long id) {
+        return orderGroupRepository.findById(id)
+                .map(orderGroup -> response(orderGroup))
+                .orElseGet(() -> Header.ERROR("NO DATA"));
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
+        OrderGroupApiRequest requestBody = request.getData();
+
+        Optional<OrderGroup> foundOrderGroup = orderGroupRepository.findById(requestBody.getId());
+
+        return foundOrderGroup.map(orderGroup -> {
+            orderGroup.setStatus(requestBody.getStatus()).
+                    setOrderType(requestBody.getOrderType()).
+                    setRevAddress(requestBody.getRevAddress()).
+                    setRevName(requestBody.getRevName()).
+                    setPaymentType(requestBody.getPaymentType()).
+                    setTotalPrice(requestBody.getTotalPrice()).
+                    setTotalQuantity(requestBody.getTotalQuantity()).
+                    setOrderAt(requestBody.getOrderAt()).
+                    setArrivalDate(requestBody.getArrivalDate()).
+                    setUser(userRepository.getOne(requestBody.getUserId()));
+            return orderGroup; //updated orderGroup returned
+        })
+                .map(modifiedOrderGroup -> orderGroupRepository.save(modifiedOrderGroup))
+                .map(updatedOrderGroup -> response(updatedOrderGroup))
+                .orElseGet(() -> Header.ERROR("NO DATA"));
+    }
+
+    @Override
+    public Header delete(Long id) {
+        return orderGroupRepository.findById(id)
+                .map(orderGroup -> {
+                    orderGroupRepository.delete(orderGroup);
+                    return Header.OK();
+                })
+                .orElseGet(() -> Header.ERROR("NO DATA"));
+    }
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup) {
+        OrderGroupApiResponse responseBody = OrderGroupApiResponse.builder()
+                .id(orderGroup.getId())
+                .status(orderGroup.getStatus())
+                .orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress())
+                .revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getRevName())
+                .totalPrice(orderGroup.getTotalPrice())
+                .totalQuantity(orderGroup.getTotalQuantity())
+                .orderAt(orderGroup.getOrderAt())
+//                .arrivalDate(orderGroup.getArrivalDate())
+                .userId(orderGroup.getUser().getId())
+                .build();
+
+        return Header.OK(responseBody);
+    }
+}
