d2a756c4f0919246d9b500715056075e7d9600a2
ORMEEHYUNGKEUN CHA <orum93@naver.com>
2021-06-10 00:22:38 +0900

    Hello World!

---
+++ mycontact/src/main/java/com/fastcampus/javaallinone/project2/mycontact/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.fastcampus.javaallinone.project2.mycontact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(value = "api/helloWorld")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
